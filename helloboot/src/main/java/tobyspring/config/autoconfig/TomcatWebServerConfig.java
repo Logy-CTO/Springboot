867cfe53a12c9f5a06f7a0f4deebd1ba5b6a10c5
taiksu hong <74459097+Logy-CTO@users.noreply.github.com>
2023-09-22 01:23:21 +0900

    9.22

---
+++ helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -0,0 +1,21 @@
+package tobyspring.config.autoconfig;
+
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.MyAutoConfiguration;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+public class TomcatWebServerConfig {
+    @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+}
---
+++ helloboot/src/main/java/tobyspring/helloboot/HelloApplication.java
@@ -0,0 +1,12 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import tobyspring.config.MySpringBootApplication;
+
+@MySpringBootApplication
+public class HelloApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+
+	}
+}
